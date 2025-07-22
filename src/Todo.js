d0da2aa3ceeceb8f299fd289e58b3db1ff029efa
vinayjain2408 <vinay98292jain@gmail.com>
2025-07-22 13:09:18 +0530

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            <h1>Welcome to the Todo App</h1>
+            <p>This is a simple Todo application built with React.</p>
+            <button onClick={() => alert('Add Todo functionality not implemented yet!')}>Add Todo</button>
+            <p>Click the button to see a message.</p>
+            <p>Explore the app and manage your tasks efficiently.</p>
+            <p>Happy coding!</p>
+            <p>Check out the source code on GitHub.</p>
+            <p>Feel free to contribute and improve the app.</p>
+            <p>Stay organized and productive with your todos.</p>
+            <h1>Todo List</h1>
+            <button>Add Todo</button>
+        </div>
+    )
+}
+
+export default Todo
